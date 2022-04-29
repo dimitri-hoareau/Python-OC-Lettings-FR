87afc849ab7b7cecf7faebc2366b41b0338af808
dimitri-hoareau <dimitrihoareau2@gmail.com>
2022-04-29 21:35:38 +0200

    rename templates

--- profiles/views.py
+++ profiles/views.py
@@ -4,7 +4,7 @@ from .models import Profile
 def index(request):
     profiles_list = Profile.objects.all()
     context = {'profiles_list': profiles_list}
-    return render(request, 'profiles_index.html', context)
+    return render(request, 'profiles/index.html', context)
 
 # Aliquam sed metus eget nisi tincidunt ornare accumsan eget lac laoreet neque quis,
 # pellentesque dui. Nullam facilisis pharetra vulputate. Sed tincidunt, dolor id facilisis
@@ -20,4 +20,4 @@ def index(request):
 def profile(request, username):
     profile = Profile.objects.get(user__username=username)
     context = {'profile': profile}
-    return render(request, 'profile.html', context)
+    return render(request, 'profiles/profile.html', context)
