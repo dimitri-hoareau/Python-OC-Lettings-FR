5b2149c374376f1e744fc354a8b2e04efb8c727b
dimitri-hoareau <dimitrihoareau2@gmail.com>
2022-05-10 18:04:36 +0200

    heroky deploy config

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,4 +1,7 @@
 import os
+from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -110,3 +113,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
